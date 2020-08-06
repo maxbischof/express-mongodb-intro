6273c69fb89a271787df30befa9f53a7fb97222a
Jeremias Erbs <jerry@badabam.de>
2020-08-06 16:34:14 +0200

    Add first routes with express

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log('hello you')
+import express from 'express'
+import db from './db.json'
+
+const server = express()
+server.use(express.json())
+
+const port = process.env.PORT ?? 4000
+
+server.get('/buddys', (req, res) => {
+  res.json(db.buddyList)
+})
+
+server.post('/energy', (req, res) => {
+  console.log(req.body)
+  res.json(req.body)
+})
+
+server.listen(port, () =>
+  console.log(`Server started on http://localhost:${port}`)
+)
